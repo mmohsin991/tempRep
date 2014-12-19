687c20e554ca9c8282a501e5b90c0fa7315eb752
mmohsin991 <muhammad.mohsin.991@gmail.com>
2014-12-19 12:57:11 +0500

    add again all files

---
+++ delegate/ViewController.swift
@@ -0,0 +1,46 @@
+//
+//  ViewController.swift
+//  delegate
+//
+//  Created by Mohsin on 18/12/2014.
+//  Copyright (c) 2014 Mohsin. All rights reserved.
+//
+
+import UIKit
+
+
+
+class ViewController: UIViewController, myDel {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if segue.identifier == "categorySeg" {
+            let desVC = segue.destinationViewController as categoryVC
+            desVC.delegate = self
+        }
+
+    }
+
+    @IBAction func gotoCategory(sender: AnyObject) {
+        performSegueWithIdentifier("categorySeg", sender: self)
+    }
+    
+    
+    func doSome(myVC: UIViewController, pushVC: UIViewController) {
+        myVC.navigationController?.popViewControllerAnimated(false)
+        
+        self.navigationController?.pushViewController(pushVC, animated: true)
+    }
+}
+
